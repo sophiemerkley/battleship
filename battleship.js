0b0034c5970f58295cd1f2511c1b30819f2e0f9e
sophie.leeanne <sophie.lamerkley@gmail.com>
2016-08-10 14:32:23 -0700

    fourth commit added counter of torpedos

--- battleship.js
+++ battleship.js
@@ -3,15 +3,28 @@ $(document).ready(function(){
   //calls makeBoard function to create the game board grid
   makeBoard();
 
-  //when click a grid space
-  $("td").on("click",
-    function(){
-      $(this).addClass("grey");
-    });
+    //when click a grid space
+    $("td").on("click",
+      //add the class of grey to the squares
+      function(){
+        if (torpedosLeft > 0) {
+          $(this).addClass("grey");
+          torpedosLeft = torpedosLeft - 1;
+          $("#torpedos").text(torpedosLeft);
+        }
+      });
+
 
 });//end of document.ready
 
 //start of model
+//create variable for torpedos left and set it to 0
+var torpedosLeft = 25;
+// Pupose: create a function to keep track of torpedos left
+//Signature: nothing --> number
+//Examples: torpedoDecrementer() --> 24;
+
+
 // Purpose: to create gameBoard using loops
 // Signature: nothing ->
 // Examples: gameBoard() -> table
