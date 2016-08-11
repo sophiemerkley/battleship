b14d6466679ca0b365c72ed28b2a6aac1f3f48f1
sophie.leeanne <sophie.lamerkley@gmail.com>
2016-08-11 10:14:44 -0700

    eighth commit added winning message and count of hits

--- battleship.js
+++ battleship.js
@@ -2,17 +2,34 @@
 $(document).ready(function(){
   //calls makeBoard function to create the game board grid
   makeBoard();
+  //calls the placeShips function to place ships on the board
   placeShips();
 
     //when click a grid space
     $("td").on("click",
+      //run a function
       function(){
+        //checks to see if there are torpedos left
         if (torpedosLeft > 0) {
+          //get the #id of the td that has been clicked
           var clicked = $(this).attr("id");
+          //splits the #id from a string into an array
           clicked.split("");
+          //prints clicked to the console for testing purposes
           console.log(clicked);
-          if (board[clicked[0]][clicked[1]] === -1) {
+          //checks to see if the td that has been clicked has a ship
+          if (board[clicked[0]][clicked[1]] === aShipIsHere) {
+            //adds a class of hit to the td
             $("#" + clicked[0] + clicked[1]).addClass("hit");
+            //increments the count of ships that have been hit
+            shipsHit = shipsHit + 1;
+            //displays the count of ships hit in the view
+            $("#hits").text(shipsHit);
+            //checks to see if 5 ships have been hit
+            if (shipsHit === 5) {
+              //add winning message
+              $("#winner").text("You WIN!");
+            }
           }
           //add the class of grey to the squares
           $(this).addClass("grey");
@@ -22,6 +39,7 @@ $(document).ready(function(){
           torpedosLeft = torpedosLeft - 1;
           //printing number of torpedos left to the dom
           $("#torpedos").text(torpedosLeft);
+
 
         }
       });
@@ -47,28 +65,27 @@ var board =[[0,0,0,0,0,0,0,0,0,0],
 
 //create variable for ships
 var ships = 0;
-var row;
-var column;
+//create variable for ships that have been hit
+var shipsHit = 0;
+//create a variable for where a ship has been placed and set the value to -1
+var aShipIsHere = -1;
+
 //Purpose: create a function to place five ships randomly on the board
 //Signature: nothing ---> return 2 numbers that correspond to positions on the board
 //Example: placeShips() ----> [0,3],[1,4],[2,6],[7,5],[1,8]
 function placeShips() {
+  //checks ships is less than five
   while (ships < 5 ){
-    row = Math.floor((Math.random()*10));
-    column = Math.floor((Math.random()*10));
+    //finds a random number for the row
+    var row = Math.floor((Math.random()*10));
+    //finds a random number for the column
+    var column = Math.floor((Math.random()*10));
+    //increments the count of ships for the while loop
     ships = ships + 1;
-    board[row][column]= -1;
+    //assigns the row and column to a ship position
+    board[row][column]= aShipIsHere;
   }
 }
-
-var shipsHit = 0;
-
-//Purpose: Create a function that takes the position shot at and sees if there was a ship there or not.
-//Signature: [x],[y] -> boolean
-//Example: shootTorpedo(x, y) -> true/false
-
-
-
 
 
 // Purpose: to create gameBoard using loops
